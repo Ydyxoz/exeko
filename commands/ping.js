2906509f265438a3e455e8333796afed221e48e9
Ydyxoz <78032273+Ydyxoz@users.noreply.github.com>
2021-01-27 18:12:47 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,5 @@
 module.exports = {
-  name: `${prefix} Beer`,
+  name: 'Beer',
   description: 'Sert une bière',
   execute(message) {
     message.channel.send('Hello dear friend, what can I do for you?')
