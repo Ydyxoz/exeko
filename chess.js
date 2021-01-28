a1be1054198f8f24e1f1b9cbe0cfba52f6abab29
Ydyxoz <78032273+Ydyxoz@users.noreply.github.com>
2021-01-28 11:23:27 +0100

    Add files via upload

---
+++ chess.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'chess',
+    description: 'Link to chess',
+    execute(message) {
+      message.channel.send('Wanna play chess? Go to https://www.chess.com/');
+    }
+  };
---
+++ wolfy.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'wolfy',
+    description: 'link to wolfy',
+    execute(message) {
+      message.channel.send('Wanna play wolfy? Go to https://wolfy.fr/play');
+    }
+  };
